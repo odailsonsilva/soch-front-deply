691919f0589e35e799ec68326b920ba3d5b63f0e
Odailson Silva <dev.odailson2021@gmail.com>
2022-08-15 17:55:11 -0300

    fix: provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { themeChakra } from 'styles/themeChakra'
 import { BrowserRouter } from 'react-router-dom'
+import styled from '@emotion/styled'
 
 import { Routes } from 'navigation/routes'
 import { Suspense } from 'react'
 import { SelectedClasseProvider } from 'context/use-selected-classe'
 import '@fontsource/spline-sans'
 
+const AppContainer = styled.div`
+  font-family: 'Spline Sans', sans-serif;
+`
+
 function App() {
   return (
     <Suspense fallback={() => 'loading...'}>
       <BrowserRouter>
         <ChakraProvider theme={themeChakra}>
-          <SelectedClasseProvider>
-            <Routes />
-          </SelectedClasseProvider>
+          <AppContainer>
+            <SelectedClasseProvider>
+              <Routes />
+            </SelectedClasseProvider>
+          </AppContainer>
         </ChakraProvider>
       </BrowserRouter>
     </Suspense>
